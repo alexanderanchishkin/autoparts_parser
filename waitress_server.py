be31a46ea5e3f8c298d16ca024bcbac3cd6e77f7
Alexander Anchishkin <alexanderanchishkin@gmail.com>
2020-04-10 02:00:36 +0300

    Refactoring waitress_server.py

--- waitress_server.py
+++ waitress_server.py
@@ -1,4 +1,5 @@
-from waitress import serve
+import waitress
+
 import main
-serve(main.app, host='0.0.0.0', port=5000, cleanup_interval=3600,
-      channel_timeout=3600, expose_tracebacks=True, threads=32)
+
+waitress.serve(main.app, host='0.0.0.0', port=5000)
