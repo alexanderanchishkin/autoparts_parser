8c9dcc2d54dcca3a0eb8232ed03cd3b604c23658
Alexander Anchishkin <alexanderanchishkin@gmail.com>
2020-04-10 11:25:08 +0300

    Excel utilities refactoring fix

--- core/io/xlsx/utilities/merge.py
+++ core/io/xlsx/utilities/merge.py
@@ -1,17 +1,17 @@
 import os.path
 
-from openpyxl import Workbook, load_workbook
+import openpyxl
 
 from config import settings
 
 
 def merge_files(files, out_name):
-    wb = Workbook()
+    wb = openpyxl.Workbook()
 
     temp_folder = settings.TEMP_XLSX_DIRECTORY
     for file in files:
         filename = os.path.join(temp_folder, file)
-        file_wb = load_workbook(filename, read_only=True)
+        file_wb = openpyxl.load_workbook(filename, read_only=True)
         file_ws = file_wb.active
         wb.create_sheet(file_ws.title)
 
@@ -20,8 +20,9 @@ def merge_files(files, out_name):
 
     output_filename = f"{settings.time_moment_name}_{out_name}.xlsx"
     output_path = os.path.join(settings.RESULTS_FOLDER, output_filename)
-    print(output_path)
+
     print(output_filename)
+
     wb.remove(wb.active)
     wb.save(output_path)
     return output_filename
